a708a657eb89077d8a999134b0274e5650e8169f
Shukur <shukuralyv@gmail.com>
2020-06-21 20:02:15 +0400

    User Screen appearance finished

--- App.js
+++ App.js
@@ -1,15 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { RootNav } from "./navigation";
 export default function App() {
   return <RootNav />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ styles/commonStyles.js
@@ -0,0 +1,9 @@
+export const shadow = {
+  shadowRadius: 2,
+  shadowOffset: {
+    width: 0,
+    height: -3,
+  },
+  shadowColor: '#000000',
+  elevation: 4,
+};
