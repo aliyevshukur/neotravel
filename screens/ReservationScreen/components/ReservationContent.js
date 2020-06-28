7f7b6c6ddd69b92a8b8674bd16f65ad15e861189
Shukur <shukuralyv@gmail.com>
2020-06-28 14:25:23 +0400

    Reservation screen polishing

--- screens/ReservationScreen/components/ReservationContent.js
+++ screens/ReservationScreen/components/ReservationContent.js
@@ -1,13 +1,31 @@
-import React, { useState, useRef } from "react";
-import { StyleSheet, ScrollView, View, Text } from "react-native";
-import { Formik } from "formik";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  SafeAreaView,
+} from "react-native";
+import { Header } from "@react-navigation/stack";
 
-import { CustomInput } from "../../../components";
+import COLORS from "../../../styles/colors";
+import { CustomInput, CustomText } from "../../../components";
 import { CreditCard } from "./CreditCard";
 import { HotelLarge } from "../../../components/cards/HotelLarge";
-import COLORS from "../../../styles/colors";
+import { CustomSvg } from "../../../components/cards/CustomSvg";
 
-export const ReservationContent = ({ stageNumber }) => {
+export const ReservationContent = (props) => {
+  const {
+    stageNumber,
+    userFormValues,
+    cardFormValues,
+    handleUserFieldChange,
+    handleCardFieldChange,
+    setIsKeyboardAvoidEnabled,
+  } = props;
+
+  const [isChecked, setIsChecked] = useState(false);
+
   const formFields = [
     { name: "firstName", placeHolder: "First Name" },
     { name: "lastName", placeHolder: "Last Name" },
@@ -18,83 +36,56 @@ export const ReservationContent = ({ stageNumber }) => {
     { name: "mobilePhone", placeHolder: "Mobile Phone" },
   ];
 
-  const [formValues, setFormValues] = useState({
-    cardNumber: "",
-    CVV: null,
-    name: "",
-  });
+  // Enable KeyboardAvoidingView if user clicked spesific field
+  const handleInputTouch = (form, name) => {
+    let isEnabled = false;
 
-  const handleFieldChange = (name, value) => {
-    if (name === "cardNumber") {
-      value = String(value);
+    if (form === "user") {
+      if (["country", "mobilePhone", "postCode"].indexOf(name) >= 0)
+        isEnabled = true;
+    } else if (form === "card") {
+      if (["name", "CVV"].indexOf(name) >= 0) isEnabled = true;
     }
 
-    if ((name === "cardNumber" || name === "CVV") && isNaN(value)) {
-      return;
-    }
-
-    setFormValues({
-      ...formValues,
-      [name]: value,
-    });
+    setIsKeyboardAvoidEnabled(isEnabled);
   };
 
   switch (stageNumber) {
     case 1:
       return (
-        <ScrollView
-          contentContainerStyle={{
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-          style={styles.form}
-        >
-          <Formik
-            initialValues={{
-              firstName: "",
-              lastName: "",
-              email: "",
-              address: "",
-              postCode: 0,
-              country: "",
-              mobilePhone: 0,
-            }}
-            onSubmit={(values) => {
-              // console.log(values);
-            }}
-          >
-            {(props) =>
-              formFields.map((input) => (
-                <CustomInput
-                  isSearch={false}
-                  isCross={false}
-                  long={true}
-                  placeholder={input.placeHolder}
-                  value={props.values[input.name]}
-                  onChangeText={props.handleChange(input.name)}
-                />
-              ))
-            }
-          </Formik>
-        </ScrollView>
+        <>
+          {formFields.map((input, index) => (
+            <CustomInput
+              key={index}
+              isSearch={false}
+              isCross={false}
+              long={true}
+              placeholder={input.placeHolder}
+              value={userFormValues[input.name]}
+              onChangeText={() => handleUserFieldChange(input.name)}
+              onTouchStart={() => handleInputTouch("user", input.name)}
+            />
+          ))}
+        </>
       );
     case 2:
       return (
-        <>
+        <View style={styles.cardContainer}>
           <CreditCard
-            cardNumber={formValues.cardNumber}
-            name={formValues.name}
-            CVV={formValues.CVV}
+            cardNumber={cardFormValues.cardNumber}
+            name={cardFormValues.name}
+            CVV={cardFormValues.CVV}
           />
-
-          <ScrollView contentContainerStyle={styles.cardForm}>
+          <View style={styles.cardForm}>
             <CustomInput
               isSearch={false}
               isCross={false}
               long={true}
               placeholder={"CardNumber"}
-              value={formValues.cardNumber}
-              onChangeText={(value) => handleFieldChange("cardNumber", value)}
+              value={cardFormValues.cardNumber}
+              onChangeText={(value) =>
+                handleCardFieldChange("cardNumber", value)
+              }
               keyboardType={"numeric"}
               maxLength={16}
             />
@@ -103,10 +94,11 @@ export const ReservationContent = ({ stageNumber }) => {
               isSearch={false}
               isCross={false}
               placeholder={"CVV"}
-              value={formValues.CVV}
-              onChangeText={(value) => handleFieldChange("CVV", value)}
-              keyboardType={"numeric"}
+              value={cardFormValues.CVV}
+              onChangeText={(value) => handleCardFieldChange("CVV", value)}
               maxLength={3}
+              keyboardType={"numeric"}
+              onTouchStart={() => handleInputTouch("card", "CVV")}
             />
 
             <CustomInput
@@ -114,24 +106,50 @@ export const ReservationContent = ({ stageNumber }) => {
               isCross={false}
               long={true}
               placeholder={"Name"}
-              value={formValues.name}
-              onChangeText={(value) => handleFieldChange("name", value)}
+              value={cardFormValues.name}
+              onChangeText={(value) => handleCardFieldChange("name", value)}
+              onTouchStart={() => handleInputTouch("card", "name")}
             />
-          </ScrollView>
-        </>
+          </View>
+          <View style={styles.saveCard}>
+            <TouchableOpacity
+              style={styles.checkBox}
+              onPress={() => setIsChecked(!isChecked)}
+            >
+              {isChecked && (
+                <CustomSvg
+                  name={"check"}
+                  gradient={true}
+                  style={{ width: 12, height: 16 }}
+                />
+              )}
+            </TouchableOpacity>
+            <CustomText style={styles.saveCardText}>
+              Save this credit card
+            </CustomText>
+          </View>
+        </View>
       );
     case 3:
       return (
         <View style={styles.checkout3}>
-          <HotelLarge isMinimal={true} />
+          <HotelLarge
+            isMinimal={true}
+            cardInfo={{
+              imgUrl:
+                "https://images.unsplash.com/photo-1444201983204-c43cbd584d93?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+            }}
+          />
           <View style={styles.orderDescription}>
             <Text style={styles.orderText}>2 People</Text>
             <Text style={styles.orderText}>Standart King Room</Text>
             <Text style={styles.orderText}>2 nights</Text>
             <Text style={styles.orderText}>Jan 18 2019 to Jan 20 2019</Text>
           </View>
-          <View style={styles.line} />
-          <Text style={styles.cost}>$1350 USD</Text>
+          <View style={styles.lineWrapper}>
+            <View style={styles.line} />
+          </View>
+          <CustomText style={styles.cost}>$1350 USD</CustomText>
         </View>
       );
     default:
@@ -140,8 +158,13 @@ export const ReservationContent = ({ stageNumber }) => {
 };
 
 const styles = StyleSheet.create({
-  form: {
-    paddingHorizontal: 20,
+  cardContainer: {
+    alignItems: "flex-start",
+  },
+  userForm: {
+    width: "100%",
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
   cardForm: {
     alignItems: "flex-end",
@@ -149,6 +172,8 @@ const styles = StyleSheet.create({
   },
   checkout3: {
     width: "100%",
+    justifyContent: "space-between",
+    padding: 25,
   },
   orderDescription: {
     marginTop: 5,
@@ -156,17 +181,39 @@ const styles = StyleSheet.create({
   orderText: {
     fontSize: 16,
     color: COLORS.grayDark,
-    lineHeight: 40,
+    lineHeight: 33,
+  },
+  lineWrapper: {
+    width: "100%",
+    alignItems: "center",
   },
   line: {
-    width: 337,
+    width: 350,
     height: 2,
     backgroundColor: COLORS.grayLight,
     marginVertical: 15,
   },
   cost: {
     fontWeight: "bold",
-    fontSize: 36,
+    fontSize: 32,
     color: COLORS.blackText,
+  },
+  saveCard: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 15,
+  },
+  checkBox: {
+    backgroundColor: "#0000",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    width: 24,
+    height: 24,
+    elevation: 2,
+    marginRight: 10,
+  },
+  saveCardText: {
+    color: COLORS.gray,
   },
 });
