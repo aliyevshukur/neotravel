989fbab7fddb74ae58618043effacebb39b8e79a
AkbarIsmayilov <akbarismayilov1@gmail.com>
2020-07-14 03:53:55 +0400

    uncompleted branch mission

--- App.js
+++ App.js
@@ -10,8 +10,11 @@ console.warn = (message) => {
   }
 };
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as eva from "@eva-design/eva";
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
+import fb from "./firebaseConfig";
 import { ApplicationProvider } from "@ui-kitten/components";
 import { AppLoading } from "expo";
 import { RootNav } from "./navigation";
@@ -26,6 +29,39 @@ import { ReservationScreen } from "./screens";
 
 export default function App() {
   const [loaded, setLoaded] = useState(false);
+
+  // const registerForPushNotifications = async () => {
+  //   //checking for existing permission
+  //   const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+  //   let finalStatus = status;
+
+  //   // asking for permissions if is not granted
+  //   if (status !== "granted") {
+  //     const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+  //     finalStatus = status;
+  //   }
+
+  //   // if permission denied
+  //   if (finalStatus !== "granted") {
+  //     return;
+  //   }
+
+  //   //get pushNotfsToken
+  //   let token = await Notifications.getExpoPushTokenAsync();
+
+  //   //add token to firebase
+
+  //   let uid = fb.auth().currentUser.uid;
+  //   fb.db().ref("users").child(uid).update({
+  //     expoPushToken: token,
+  //   });
+  //   console.log(token, "---token");
+  //   console.log(uid, "---uid");
+  // };
+
+  // useEffect(() => {
+  //   registerForPushNotifications();
+  // }, []);
   if (!loaded) {
     return (
       <AppLoading
@@ -35,16 +71,17 @@ export default function App() {
       />
     );
   }
+
   return (
     <ApplicationProvider {...eva} theme={eva.light} mapping={mapping}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-        <StatusBar
-          // hidden={true}
-          backgroundColor={"rgba(0,0,0,0.5)"}
-          translucent={true}
-        />
-        <RootNav />
+          <StatusBar
+            // hidden={true}
+            backgroundColor={"rgba(0,0,0,0.5)"}
+            translucent={true}
+          />
+          <RootNav />
         </PersistGate>
       </Provider>
     </ApplicationProvider>
